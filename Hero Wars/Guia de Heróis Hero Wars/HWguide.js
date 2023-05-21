4dc0c4fe54d8ffdf576628a037c4d30f560362fe
Alexandre221178 <133829010+Alexandre221178@users.noreply.github.com>
2023-05-21 09:54:16 -0300

    pastas fix 2

---
+++ Hero Wars/Guia de Heróis Hero Wars/HWguide.js
@@ -0,0 +1,34 @@
+var menu = document.querySelector('nav ul');
+var menuBar = document.querySelector('nav .menu-icon');
+var iconMenu = document.querySelector('nav .menu-icon img');
+
+menuBar.addEventListener('click',function(){
+
+    if (iconMenu.getAttribute("src") == '../imagens/menu.png') {
+        iconMenu.setAttribute("src","../imagens/close.png");
+    }else{
+        iconMenu.setAttribute("src","../imagens/menu.png");
+    }
+    
+    menu.classList.toggle('active')
+});
+
+(() => {
+    if (!localStorage.pureJavaScriptCookies) {
+        document.querySelector(".box-cookies").classList.remove('hide');
+    }
+
+    const acceptCookies = () => {
+        document.querySelector(".box-cookies").classList.add('hide');
+        localStorage.setItem("pureJavaScriptCookies", "accept");
+    };
+
+    const btnCookies = document.querySelector(".btn-cookies");
+
+    btnCookies.addEventListener('click', acceptCookies);
+})();
+
+let text = document.lastModified;
+document.getElementById("demo").innerHTML = text;
+document.getElementById("demo").style.color = "#18a84d";
+
