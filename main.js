b3830c71f9fcbdbf5df726ae53da12f9ecc8eb55
Alexandre221178 <133829010+Alexandre221178@users.noreply.github.com>
2023-06-09 21:09:43 -0300

    fix js menu

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var iconMenu = document.querySelector('nav .menu-icon img');
 
 menuBar.addEventListener('click',function(){
 
-    if (iconMenu.getAttribute("src") == 'imagens/menu.png') {
-        iconMenu.setAttribute("src","imagens/close.png");
-    }else{
+    if (iconMenu.getAttribute("src") == 'imagens/close.png') {
         iconMenu.setAttribute("src","imagens/menu.png");
+    }else{
+        iconMenu.setAttribute("src","imagens/close.png");
     }
     
     menu.classList.toggle('active')
