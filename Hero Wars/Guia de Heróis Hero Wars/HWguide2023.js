4fe68a9408f1003fc8388ce5dc21fbdbcf481fc5
Alexandre221178 <133829010+Alexandre221178@users.noreply.github.com>
2023-07-26 19:37:36 -0300

    teste data arachne

---
+++ Hero Wars/Guia de Heróis Hero Wars/HWguide2023.js
@@ -0,0 +1,33 @@
+var menu = document.querySelector('nav ul');
+var menuBar = document.querySelector('nav .menu-icon');
+var iconMenu = document.querySelector('nav .menu-icon img');
+
+menuBar.addEventListener('click',function(){
+
+    if (iconMenu.getAttribute("src") == '../../imagens/close.png') {
+        iconMenu.setAttribute("src","../../imagens/menu.png");
+    }else{
+        iconMenu.setAttribute("src","../../imagens/close.png");
+    }
+    
+    menu.classList.toggle('active')
+});
+
+(() => {
+    if (!localStorage.pureJavaScriptCookies) {
+        document.querySelector(".box-cookies").classList.remove('hide');
+    }
+
+    const acceptCookies = () => {
+        document.querySelector(".box-cookies").classList.add('hide');
+        localStorage.setItem("pureJavaScriptCookies", "accept");
+    };
+
+    const btnCookies = document.querySelector(".btn-cookies");
+
+    btnCookies.addEventListener('click', acceptCookies);
+})();
+
+
+
+
