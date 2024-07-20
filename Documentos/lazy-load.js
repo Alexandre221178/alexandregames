e156e2909e44d48b78aefc7ab937b8708b31c923
Alexandre221178 <alexandredomingos2211@gmail.com>
2024-07-20 20:05:50 -0300

    lazy

--- Documentos/lazy-load.js
+++ Documentos/lazy-load.js
@@ -1,3 +1,45 @@
+ //  cookies alexandregames
+document.addEventListener("DOMContentLoaded", function() {
+  const acceptCookiesBtn = document.getElementById('accept-cookies');
+  const rejectCookiesBtn = document.getElementById('reject-cookies');
+  const cookieBanner = document.getElementById('cookie-banner');
+
+  acceptCookiesBtn.addEventListener('click', function() {
+      setCookie('cookie_consent', 'accepted', 365);
+      cookieBanner.style.display = 'none';
+  });
+
+  rejectCookiesBtn.addEventListener('click', function() {
+      setCookie('cookie_consent', 'rejected', 365);
+      cookieBanner.style.display = 'none';
+  });
+
+  function setCookie(name, value, days) {
+      const expires = new Date();
+      expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+      document.cookie = name + '=' + value + ';expires=' + expires.toUTCString() + ';path=/';
+  }
+
+  function getCookie(name) {
+      const cookieArray = document.cookie.split(';');
+      for (let i = 0; i < cookieArray.length; i++) {
+          const cookiePair = cookieArray[i].split('=');
+          if (name === cookiePair[0].trim()) {
+              return decodeURIComponent(cookiePair[1]);
+          }
+      }
+      return null;
+  }
+
+  const consent = getCookie('cookie_consent');
+  if (consent === 'accepted' || consent === 'rejected') {
+      cookieBanner.style.display = 'none';
+  }
+});
+//  fim do cookies alexandregames
+ 
+ 
+ 
  // Função para carregar a segunda metade do conteúdo após um atraso
  function loadSecondHalf() {
   document.getElementById('second-half').classList.remove('hidden');
@@ -5,3 +47,4 @@
 
 // Simula um atraso antes de carregar a segunda metade do conteúdo
 setTimeout(loadSecondHalf, 200); // Atraso de 2 segundos
+
