865ff29c49cb9642dffd8551f3eac769b7f87fda
Alexandre221178 <133829010+Alexandre221178@users.noreply.github.com>
2024-07-22 11:18:29 -0300

    Update nav3-pt.js

--- Documentos/nav3-pt.js
+++ Documentos/nav3-pt.js
@@ -99,19 +99,3 @@ document.addEventListener('DOMContentLoaded', () => {
     displayModificationDate();
 });
 
-// Código para ajustar a altura do container de anúncios
-window.addEventListener('load', function() {
-    var adContainer = document.getElementById('ad-container');
-    var adElement = adContainer.querySelector('.adsbygoogle');
-
-    function adjustAdHeight() {
-        if (adElement && adElement.clientHeight > 0) {
-            adContainer.style.height = adElement.clientHeight + 'px';
-        }
-    }
-
-    adjustAdHeight();
-
-    // Verifica periodicamente se a altura do anúncio mudou
-    setInterval(adjustAdHeight, 500);
-});
