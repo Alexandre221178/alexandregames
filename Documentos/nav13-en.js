c5ae62391de8026f0130a10861d8520f46bca301
Alexandre221178 <alexandredomingos2211@gmail.com>
2024-07-23 23:05:33 -0300

    fix nav13 en

--- Documentos/nav13-en.js
+++ Documentos/nav13-en.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navHTML = `
         <div class="logo">
-            <a href="https://alexandregames.com/index-en.html"><img src="https://alexandregames.com/imagens/exandregames318x318-removebg.webp" alt="Alexandre Games Mobile Logo" title="Alexandre Games Mobile Logo"></a>
+            <a href="https://alexandregames.com"><img src="https://alexandregames.com/imagens/exandregames318x318-removebg.webp" alt="Alexandre Games Mobile Logo" title="Alexandre Games Mobile Logo"></a>
         </div>                     
         <ul>                     
             <div class="dropdown">
