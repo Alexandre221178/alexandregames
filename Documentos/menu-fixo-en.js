1753d02d8f96579e8c5435d99b0c8299e99d9aa8
Alexandre221178 <alexandredomingos2211@gmail.com>
2024-11-03 20:20:25 -0300

    ad menu

--- Documentos/menu-fixo-en.js
+++ Documentos/menu-fixo-en.js
@@ -1,10 +1,11 @@
 // Define o conteúdo HTML do menu
 const menuContent = `
     <h6>Alexandre Games</h6>
-    <a href="./herowars-alliance-en.html">Hero Wars Alliance</a>
-    <a href="./MobileLegends-English.html">Mobile Legends</a>
-    <a href="./roblox-us.html">Roblox</a>
-    <a href="./top-troops/top-troops-en-home.html">Top Troops</a>
+    <a href="https://alexandregames.com/herowars-alliance-en.html">Hero Wars Alliance</a>
+    <a href="https://alexandregames.com/MobileLegends-English.html">Mobile Legends</a>
+    <a href="https://alexandregames.com/ragnarok-origin/codes-ragnarok-us.html">Ragnarok ROO</a>
+    <a href="https://alexandregames.com/roblox-us.html">Roblox</a>
+    <a href="https://alexandregames.com/top-troops/top-troops-en-home.html">Top Troops</a>
 `;
 
 // Seleciona a div existente com a classe "menu-fixo"
