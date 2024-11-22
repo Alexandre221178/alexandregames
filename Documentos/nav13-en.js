d492ffca17e1fa6369c8ee53bd295b81732b611f
Alexandre221178 <alexandredomingos2211@gmail.com>
2024-11-22 20:25:43 -0300

    ok

--- Documentos/nav13-en.js
+++ Documentos/nav13-en.js
@@ -11,10 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
                  
                 <a href="https://alexandregames.com/herowars-alliance-en.html">HWA - Home</a>
                     <a href="https://alexandregames.com/hero-wars-alliance/menu-best-teams-en-hwa.html">Best Teams Guide</a>
+                    <a href="https://alexandregames.com/Hero%20Wars/calendar-event-en-hwa.html">Calendar & Events</a>
                     <a href="https://alexandregames.com/Hero%20Wars%20English/Heroes%20Guide%20Hero%20Wars.html">Heroes Guide</a>
-                    <a href="https://alexandregames.com/hero-wars-alliance/menu-review-en-hwa.html">Reviews</a>
+                    <a href="https://alexandregames.com/hero-wars-alliance/menu-review-en-hwa.html">Skins Reviews</a>
+                    <a href="https://alexandregames.com/Hero%20Wars%20English/Titans%20Guide%20Hero%20Wars.html">Tactics & Secrets</a>
                     <a href="https://alexandregames.com/Hero%20Wars%20English/Titans%20Guide%20Hero%20Wars.html">Titans Guide</a>
-                   
                     
                     
                    
