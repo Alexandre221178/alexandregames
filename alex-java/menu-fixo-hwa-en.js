d8e786da2089efb1a8394c3c1af9eec0501981e3
Alexandre221178 <alexandredomingos2211@gmail.com>
2024-11-23 08:07:57 -0300

    new menus

--- alex-java/menu-fixo-hwa-en.js
+++ alex-java/menu-fixo-hwa-en.js
@@ -1,12 +1,14 @@
 // Define o conteúdo HTML do menu
 const menuContent = `
-    <h6>Alexandre Games</h6>
-    <a href="https://alexandregames.com/herowars-alliance-en.html">Hero Wars Alliance</a>
-    <a href="https://alexandregames.com/MobileLegends-English.html">Mobile Legends</a>
-    <a href="https://alexandregames.com/ragnarok-origin/codes-ragnarok-us.html">Ragnarok ROO</a>
-    <a href="https://alexandregames.com/roblox-us.html">Roblox</a>
-    <a href="https://alexandregames.com/top-troops/top-troops-en-home.html">Top Troops</a>
-    <a href="https://alexandregames.com/throne-and-liberty/menu-throne-and-liberty/menu-throne-and-liberty-en.html">Throne and Liberty</a>
+    <h6>Hero Wars Alliance</h6>
+    <a href="https://alexandregames.com/herowars-alliance-en.html">HWA - Home</a>
+                    <a href="https://alexandregames.com/hero-wars-alliance/menu-best-teams-en-hwa.html">Best Teams Guide</a>
+                    <a href="https://alexandregames.com/Hero%20Wars/calendar-event-en-hwa.html">Calendar & Events</a>
+                    <a href="https://alexandregames.com/Hero%20Wars%20English/Heroes%20Guide%20Hero%20Wars.html">Heroes Guide</a>
+                    <a href="https://alexandregames.com/hero-wars-alliance/menu-review-en-hwa.html">Skins Reviews</a>
+                    <a href="https://alexandregames.com/Hero%20Wars%20English/Titans%20Guide%20Hero%20Wars.html">Tactics & Secrets</a>
+                    <a href="https://alexandregames.com/Hero%20Wars%20English/Titans%20Guide%20Hero%20Wars.html">Titans Guide</a>
+                    
 `;
 
 // Seleciona a div existente com a classe "menu-fixo"
