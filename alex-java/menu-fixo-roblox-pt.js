177cb9df8a9b0bf821a61c2f05875d72de0e1bc9
Alexandre221178 <alexandredomingos2211@gmail.com>
2024-12-01 15:50:43 -0300

    fix menus

--- alex-java/menu-fixo-roblox-pt.js
+++ alex-java/menu-fixo-roblox-pt.js
@@ -1,20 +1,20 @@
 // Define o conteúdo HTML do menu
 const menuContent = `
   
-    <h6><a href="https://alexandregames.com/roblox-us.html">Roblox: Mais Visitados</a></h6>
+    <h6><a href="https://alexandregames.com/roblox-br.html">Roblox: Mais Visitados</a></h6>
 
-<a href="https://alexandregames.com/roblox/roblox-us/brookhaven-guide-roblox.html">Explorando Brookhaven</a>
-<a href="https://alexandregames.com/roblox/roblox-us/secret-tricks-to-win-at-roblox.html">13 Truques Secretos para Roblox</a>
-<a href="https://alexandregames.com/roblox/roblox-us/how-to-get-voice-chat-en-roblox.html">Como Ativar o Chat de Voz</a>
-<a href="https://alexandregames.com/roblox/what-abc-means-roblox-en.html">O Que Significa "ABC" no Roblox</a>
-<a href="https://alexandregames.com/roblox/redeem-code-en-roblox.html">Códigos do Roblox</a>
-<a href="https://alexandregames.com/roblox/roblox-us/flee-the-facility-tips.html">Marretão: Fuja da Instalação</a>
-<a href="https://alexandregames.com/roblox/roblox-us/roblox-man-face-en.html">Rosto Masculino do Roblox</a>
-<a href="https://alexandregames.com/roblox/roblox-us/murder-guide-roblox.html">Murder Mystery</a>
+<a href="https://alexandregames.com/roblox/roblox-br/brookhaven-tutorial-roblox.html">Explorando Brookhaven</a>
+<a href="https://alexandregames.com/roblox/roblox-br/truques-secretos-para-vencer-no-roblox.html">13 Truques Secretos para Roblox</a>
+<a href="https://alexandregames.com/roblox/roblox-br/how-to-get-voice-chat-pt-roblox.html">Como Ativar o Chat de Voz</a>
+<a href="https://alexandregames.com/roblox/what-abc-means-roblox-pt.html">O Que Significa "ABC" no Roblox</a>
+<a href="https://alexandregames.com/roblox/redeem-code-pt-roblox.html">Códigos do Roblox</a>
+<a href="https://alexandregames.com/roblox/roblox-br/fujadainstalacao-dicas-marretao.html">Marretão: Fuja da Instalação</a>
+<a href="https://alexandregames.com/roblox/roblox-br/roblox-man-face-pt.html">Rosto Masculino do Roblox</a>
+<a href="https://alexandregames.com/roblox/roblox-br/murder-tutorial-roblox.html">Murder Mystery</a>
 
 <br>
-<h6><a href="https://alexandregames.com/roblox-us.html">Roblox: Mais Recentes</a></h6>
-<a href="https://alexandregames.com/roblox/best-roblox-horror-games-en.html">Melhores Jogos de Terror - Roblox</a>
+<h6><a href="https://alexandregames.com/roblox-br.html">Roblox: Mais Recentes</a></h6>
+<a href="https://alexandregames.com/roblox/best-roblox-horror-games-pt.html">Melhores Jogos de Terror - Roblox</a>
     
 `;
 
