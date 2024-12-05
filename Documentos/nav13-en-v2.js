56ae695a0fd85b2a4a7e22e26be9892b3ad6f98a
Alexandre221178 <alexandredomingos2211@gmail.com>
2024-12-04 21:49:34 -0300

    teste novo nav

---
+++ Documentos/nav13-en-v2.js
@@ -0,0 +1,131 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navHTML = `
+        <div class="logo">
+            <a href="https://alexandregames.com"><img src="https://alexandregames.com/imagens/exandregames318x318-removebg.webp" alt="Alexandre Games Mobile Logo" title="Alexandre Games Mobile Logo"></a>
+        </div>
+        
+        <ul> 
+                <div class="dropdown">
+                <button class="dropbtn">Hero Wars Alliance</button>
+                <div class="dropdown-content">
+                 
+                <a href="https://alexandregames.com/herowars-alliance-en.html">HWA - Home</a>
+                    <a href="https://alexandregames.com/hero-wars-alliance/menu-best-teams-en-hwa.html">Best Teams Guide</a>
+                    <a href="https://alexandregames.com/Hero%20Wars/calendar-event-en-hwa.html">Calendar & Events</a>
+                    <a href="https://alexandregames.com/Hero%20Wars%20English/Heroes%20Guide%20Hero%20Wars.html">Heroes Guide</a>
+                    <a href="https://alexandregames.com/hero-wars-alliance/event-hwa/herosway-en-hwa.html">Hero's Way <br>Nov. 25 - Dec. 22</a>  
+                    <a href="https://alexandregames.com/hero-wars-alliance/menu-review-en-hwa.html">Skins Reviews</a>
+                    <a href="https://alexandregames.com/hero-wars-alliance/menu-tactics-secrets-hwa-en.html">Tactics & Secrets</a>
+                    <a href="https://alexandregames.com/Hero%20Wars%20English/Titans%20Guide%20Hero%20Wars.html">Titans Guide</a>
+                    
+                   <br><br><br>
+                   
+                </div>
+            </div>              
+            <div class="dropdown">
+                <button class="dropbtn"><a href="https://alexandregames.com/MobileLegends-English.html">Mobile Legends</a></button> 
+            </div> 
+            <div class="dropdown">
+                <button class="dropbtn"><a href="https://alexandregames.com/roblox-us.html">Roblox PC/Mobile</a></button>
+            </div> 
+            <div class="dropdown">
+               <button><a href="https://alexandregames.com/top-troops/top-troops-en-home.html">Top Troops</a></button>
+            </div>
+            <div class="dropdown">
+               <button><a href="https://alexandregames.com/throne-and-liberty/menu-throne-and-liberty/menu-throne-and-liberty-en.html">Throne and Liberty</a></button>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">More Games</button>
+                <div class="dropdown-content">
+                    <a href="https://alexandregames.com/ragnarok-origin/codes-ragnarok-us.html">Ragnarok ROO</a>
+                    <a>_____________</a>
+                    <a href="https://alexandregames.com/Documentos/Contact.html">Contact</a>
+                    <a href="https://alexandregames.com/Documentos/disclaimer.html">Disclaimer</a>
+                    <a href="https://alexandregames.com/Documentos/PrivacyPolicy.html">Privacy Policy</a>
+                    <a href="https://alexandregames.com/Documentos/about.html">About Us</a>
+                    <a href="https://alexandregames.com/Documentos/TermsofUse.html">Terms of Use</a>
+                    <br><br><br>
+                </div>
+            </div>
+           <div class="dropdown">
+        <button class="language-switch">
+            <img src="https://alexandregames.com/imagens/flag-brazil-32x32.png" alt="Brazil flag" title="Brazil flag" style="width:24px;height:16px">
+            <strong>PT</strong>
+        </button>
+    </div>
+        </ul>
+        <div class="menu-icon">
+            <img src="https://alexandregames.com/imagens/menu.png" alt="Menu" style="width:48px;height:48px;">
+        </div>
+    `;
+
+    const navElement = document.createElement('nav');
+    navElement.innerHTML = navHTML;
+    document.querySelector('#menu-container').prepend(navElement);
+
+    // Captura o botão de troca de idioma
+const langButton = document.querySelector('.language-switch');
+
+// Captura o link definido no HTML
+const langLink = document.querySelector('.language-link');
+
+if (langButton && langLink) {
+    // Adiciona o link ao botão
+    langButton.addEventListener('click', () => {
+        window.location.href = langLink.href;
+    });
+}
+
+    const menu = document.querySelector('nav ul');
+    const menuBar = document.querySelector('nav .menu-icon');
+    const iconMenu = document.querySelector('nav .menu-icon img');
+
+    menuBar.addEventListener('click', function() {
+        if (iconMenu.getAttribute("src") === 'https://alexandregames.com/imagens/close.webp') {
+            iconMenu.setAttribute("src", "https://alexandregames.com/imagens/menu.png");
+        } else {
+            iconMenu.setAttribute("src", "https://alexandregames.com/imagens/close.webp");
+        }
+        
+        menu.classList.toggle('active');
+    });
+});
+
+
+
+
+
+
+
+// Function to extract and display modification date
+function displayModificationDate() {
+    var metaTags = document.getElementsByTagName('meta');
+    var modificationDate;
+
+    // Array with month names
+    var months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    // Find the meta tag with property="article:modified_time"
+    for (var i = 0; i < metaTags.length; i++) {
+        if (metaTags[i].getAttribute('property') === 'article:modified_time') {
+            modificationDate = metaTags[i].getAttribute('content');
+            break;
+        }
+    }
+
+    // Display the modification date on the page
+    if (modificationDate) {
+        var formattedDate = new Date(modificationDate);
+        var day = formattedDate.getDate();
+        var month = months[formattedDate.getMonth()]; // Get the month name
+        var year = formattedDate.getFullYear();
+        var dateElement = document.getElementById('data-modificacao');
+        dateElement.textContent = 'Last updated: ' + month + ' ' + day + ', ' + year;
+    }
+}
+
+// Call the function when the page loads
+window.onload = displayModificationDate;
---
+++ Documentos/nav13-pt-v2.js
@@ -0,0 +1,129 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navHTML = `
+        <div class="logo">
+            <a href="https://alexandregames.com/index-pt.html"><img src="https://alexandregames.com/imagens/exandregames318x318-removebg.webp" alt="logo Alexandre Games Mobile" title="logo Alexandre Games Mobile"></a>
+        </div>                     
+        <ul>                     
+            <div class="dropdown">
+                <button class="dropbtn">Hero Wars Alliance</button>
+                <div class="dropdown-content">
+                 
+                <a href="https://alexandregames.com/herowars-alliance-pt.html">HWA - Início</a>  
+                <a href="https://alexandregames.com/hero-wars-alliance/menu-best-teams-pt-hwa.html">Guia de Melhores Times</a>  
+                <a href="https://alexandregames.com/Hero%20Wars/calendar-event-pt-hwa.html">Calendário & Eventos</a>
+                <a href="https://alexandregames.com/hero-wars-alliance/event-hwa/herosway-pt-hwa.html">Caminho do Herói <br>Nov. 25 a Dez. 22</a>  
+                <a href="https://alexandregames.com/Hero%20Wars/Guiadeherois.html">Guia de Heróis</a>  
+                <a href="https://alexandregames.com/hero-wars-alliance/menu-review-pt-hwa.html">Análises de Skins</a>  
+                <a href="https://alexandregames.com/hero-wars-alliance/menu-tactics-secrets-hwa-en.html">Táticas & Segredos</a>  
+                <a href="https://alexandregames.com/Hero%20Wars/Guia%20de%20Titans.html">Guia de Titãs</a>  
+                    
+                   <br><br><br>
+                </div>  
+            </div>           
+            <div class="dropdown">
+                <button class="dropbtn"><a href="https://alexandregames.com/MobileLegends.html">Mobile Legends</a></button>
+                
+            </div> 
+            <div class="dropdown">
+                <button class="dropbtn"><a href="https://alexandregames.com/roblox-br.html">Roblox PC/Mobile</a></button>
+                
+            </div> 
+<div class="dropdown">
+            <button><a href="https://alexandregames.com/top-troops/top-troops-pt-home.html">Top Troops</a></button> 
+            </div>
+
+             <div class="dropdown">
+               <button><a href="https://alexandregames.com/throne-and-liberty/menu-throne-and-liberty/menu-throne-and-liberty-pt.html">Throne and Liberty</a></button>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Mais Jogos</button>
+                <div class="dropdown-content">
+                    <a href="https://alexandregames.com/ragnarok-origin/codigos-ragnarok-br.html">Ragnarok ROO</a>
+                    <a>_____________</a>
+                    <a href="https://alexandregames.com/Documentos/Contato.html">Contato</a>
+                    <a href="https://alexandregames.com/Documentos/aviso-legal.html">Isenção de Responsabilidade</a>
+                    <a href="https://alexandregames.com/Documentos/Pol%C3%ADtica%20de%20privacidade.html">Política de Privacidade</a>
+                    <a href="https://alexandregames.com/Documentos/Sobre.html">Sobre Nós</a>
+                    <a href="https://alexandregames.com/Documentos/Termos%20de%20uso.html">Termos de Uso</a>
+                    <br><br><br>
+                </div>
+            </div>
+        <div class="dropdown">
+        <button class="language-switch">
+            <img src="https://alexandregames.com/imagens/flag-us-32x32.png" alt="bandeira USA" title="bandeira" USA style="width:24px;height:16px" >
+            <strong>EN</strong>
+        </button>
+    </div>
+        </ul>
+        <div class="menu-icon">
+            <img src="https://alexandregames.com/imagens/menu.png" alt="Menu" style="width:48px;height:48px;">
+        </div>
+    `;
+
+    const navElement = document.createElement('nav');
+    navElement.innerHTML = navHTML;
+    document.querySelector('#menu-container').prepend(navElement);
+
+   // Captura o botão de troca de idioma
+const langButton = document.querySelector('.language-switch');
+
+// Captura o link definido no HTML
+const langLink = document.querySelector('.language-link');
+
+if (langButton && langLink) {
+    // Adiciona o link ao botão
+    langButton.addEventListener('click', () => {
+        window.location.href = langLink.href;
+    });
+}
+
+
+
+ // Adiciona o nav
+    const menu = document.querySelector('nav ul');
+    const menuBar = document.querySelector('nav .menu-icon');
+    const iconMenu = document.querySelector('nav .menu-icon img');
+
+    menuBar.addEventListener('click', function() {
+        if (iconMenu.getAttribute("src") === 'https://alexandregames.com/imagens/close.webp') {
+            iconMenu.setAttribute("src", "https://alexandregames.com/imagens/menu.png");
+        } else {
+            iconMenu.setAttribute("src", "https://alexandregames.com/imagens/close.webp");
+        }
+        
+        menu.classList.toggle('active');
+    });
+});
+
+// Function to extract and display modification date
+function displayModificationDate() {
+    var metaTags = document.getElementsByTagName('meta');
+    var modificationDate;
+
+    // Array com nomes dos meses
+    var months = [
+        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    ];
+
+    // Encontrar a meta tag com property="article:modified_time"
+    for (var i = 0; i < metaTags.length; i++) {
+        if (metaTags[i].getAttribute('property') === 'article:modified_time') {
+            modificationDate = metaTags[i].getAttribute('content');
+            break;
+        }
+    }
+
+    // Exibir a data de modificação na página
+    if (modificationDate) {
+        var formattedDate = new Date(modificationDate);
+        var day = formattedDate.getDate();
+        var month = months[formattedDate.getMonth()]; // Obter o nome do mês
+        var year = formattedDate.getFullYear();
+        var dateElement = document.getElementById('data-modificacao');
+        dateElement.textContent = 'Última atualização: ' + day + ' de ' + month + ' de ' + year;
+    }
+}
+
+// Chamar a função quando a página carregar
+window.onload = displayModificationDate;
