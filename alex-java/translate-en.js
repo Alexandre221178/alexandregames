d2b2d536093e54f0b04123561988a5f0dcd12a6e
Alexandre221178 <alexandredomingos2211@gmail.com>
2025-03-13 20:00:43 -0300

    testes ad

---
+++ alex-java/translate-en.js
@@ -0,0 +1,10 @@
+function translatePage() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    document.body.appendChild(script);
+
+    window.googleTranslateElementInit = function() {
+        new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+    };
+}
