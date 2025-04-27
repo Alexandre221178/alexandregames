b679165ff0ac7169a478a8f2dda0db0c585c0515
Alexandre221178 <alexandredomingos2211@gmail.com>
2025-04-27 14:16:05 -0300

    update nav

--- Documentos/nav13-en-v2.js
+++ Documentos/nav13-en-v2.js
@@ -1,102 +1,119 @@
  // atualizar somente os nav13-en-v2.js e nav13-pt-v2.js esses são com a bandeira no nav
 document.addEventListener('DOMContentLoaded', () => {
     const navHTML = `
-        <div class="logo">
-            <a href="https://alexandregames.com"><img src="https://alexandregames.com/imagens/exandregames318x318-removebg.webp" alt="Alexandre Games Mobile Logo" title="Alexandre Games Mobile Logo" width="50" height="50"></a>
-        </div>
+<div class="logo">
+    <a href="https://alexandregames.com"><img src="https://alexandregames.com/imagens/exandregames318x318-removebg.webp" alt="Alexandre Games Mobile Logo" title="Alexandre Games Mobile Logo" width="50" height="50"></a>
+</div>
+
+<ul> 
+
+<div class="dropdown">
+    <button class="dropbtn">Gameplay Reviews</button>
+    <div class="dropdown-content"><br>
+    <a href="https://alexandregames.com/gaming-guide/review/appgallery-review-en.html">Install AppGallery on Android for Hero Wars Discounts</a>   
+    <a href="https://alexandregames.com/gaming-guide/review/echoes-of-eternity-game-review-en.html">Echoes of Eternity</a>
+    <a href="https://alexandregames.com/hero-wars-alliance/guide/how-to-play-hero-wars-on-pc-and-mac-en.html">Play Hero Wars Alliance on PC & Mac</a> 
+    <a href="https://alexandregames.com/gaming-guide/review/tales-of-wind-review-en.html">Tales of Wind: RR</a>
+    <a href="https://alexandregames.com/gaming-guide/review/the-dragon-odyssey-gameplay-en.html">The Dragon Odyssey</a>                    
         
-        <ul> 
+        <br><br><br>
+            <br><br><br>
+    </div>
+</div>    
 
-        <div class="dropdown">
-            <button class="dropbtn">Gameplay Reviews</button>
-            <div class="dropdown-content"><br>
-            <a href="https://alexandregames.com/gaming-guide/review/appgallery-review-en.html">Install AppGallery on Android for Hero Wars Discounts</a>   
-            <a href="https://alexandregames.com/gaming-guide/review/echoes-of-eternity-game-review-en.html">Echoes of Eternity</a>
-            <a href="https://alexandregames.com/hero-wars-alliance/guide/how-to-play-hero-wars-on-pc-and-mac-en.html">Play Hero Wars Alliance on PC & Mac</a> 
-            <a href="https://alexandregames.com/gaming-guide/review/tales-of-wind-review-en.html">Tales of Wind: RR</a>
-            <a href="https://alexandregames.com/gaming-guide/review/the-dragon-odyssey-gameplay-en.html">The Dragon Odyssey</a>                    
-                
-                <br><br><br>
-                  <br><br><br>
-            </div>
-        </div>    
 
+
+    <div class="dropdown">
+
+    <button class="dropbtn">Hero Wars Alliance</button>
+    <div class="dropdown-content"> <br>                
+        <a href="https://alexandregames.com/herowars-alliance-en.html">HWA - Home</a>
+        <a href="https://alexandregames.com/hero-wars-alliance/menu-best-teams-en-hwa.html">Best Teams Guide</a>
+        <a href="https://alexandregames.com/Hero%20Wars/calendar-event-en-hwa.html">Calendar & Events</a>
+        <a href="https://alexandregames.com/Hero%20Wars%20English/Heroes%20Guide%20Hero%20Wars.html">Heroes Guide</a>
+        <a href="https://alexandregames.com/hero-wars-alliance/event-hwa/herosway-en-hwa.html">Hero's Way</a>  
+        <a href="https://alexandregames.com/hero-wars-alliance/menu-review-en-hwa.html">Skins Reviews</a>
+        <a href="https://alexandregames.com/hero-wars-alliance/menu-tactics-secrets-hwa-en.html">Tactics & Secrets</a>
+        <a href="https://alexandregames.com/Hero%20Wars/articles-herowars/tierlist-en-pvp-herowars.html">Tier List</a>
+        <a href="https://alexandregames.com/Hero%20Wars%20English/Titans%20Guide%20Hero%20Wars.html">Titans Guide</a>
+        
+        
+        <br><br><br><br>
+        
+    </div>
+</div>  
+
+<div class="dropdown">
+
+    <button class="dropbtn">Hero Wars: Dominion Era</button>
+    <div class="dropdown-content"> <br>  
+
+        <a href="https://alexandregames.com/hero-wars-dominion-era/guide/totems-guide-en.html">Best Totems Guide</a>
+        <a href="https://alexandregames.com/hero-wars-dominion-era/guide/pets-tier-list-en.html">PET Tier List</a>
        
-
-            <div class="dropdown">
-
-            <button class="dropbtn">Hero Wars Alliance</button>
-            <div class="dropdown-content"> <br>                
-                <a href="https://alexandregames.com/herowars-alliance-en.html">HWA - Home</a>
-                <a href="https://alexandregames.com/hero-wars-alliance/menu-best-teams-en-hwa.html">Best Teams Guide</a>
-                <a href="https://alexandregames.com/Hero%20Wars/calendar-event-en-hwa.html">Calendar & Events</a>
-                <a href="https://alexandregames.com/Hero%20Wars%20English/Heroes%20Guide%20Hero%20Wars.html">Heroes Guide</a>
-                <a href="https://alexandregames.com/hero-wars-alliance/event-hwa/herosway-en-hwa.html">Hero's Way</a>  
-                <a href="https://alexandregames.com/hero-wars-alliance/menu-review-en-hwa.html">Skins Reviews</a>
-                <a href="https://alexandregames.com/hero-wars-alliance/menu-tactics-secrets-hwa-en.html">Tactics & Secrets</a>
-                <a href="https://alexandregames.com/Hero%20Wars/articles-herowars/tierlist-en-pvp-herowars.html">Tier List</a>
-                <a href="https://alexandregames.com/Hero%20Wars%20English/Titans%20Guide%20Hero%20Wars.html">Titans Guide</a>
-                
-                
-                <br><br><br><br>
-                
-            </div>
-        </div>  
-
-        <div class="dropdown">
-            <button class="dropbtn">Mobile Legends</button>
-            <div class="dropdown-content"><br>
-                
-            <a href="https://alexandregames.com/MobileLegends-English.html">Heroes Guide</a>
-            <a href="https://alexandregames.com/mobile-legends/article-mlbb/tier-list-mlbb-en.html">MLBB Tier List</a>
-            <a href="https://alexandregames.com/mobile-legends/article-mlbb/gold-lane-guide-en-mlbb.html">Gold Lane Guide</a>
-            <a href="https://alexandregames.com/mobile-legends/article-mlbb/roam-guide-mlbb-en.html">Roam Lane Guide</a>          
-                                    
-                
-                <br><br><br>
-            </div>
-        </div>      
-
-        <div class="dropdown">
-            <button class="dropbtn"><a href="https://alexandregames.com/roblox-us.html">Roblox PC/Mobile</a></button>
-        </div> 
-
- <div class="dropdown">
-            <button class="dropbtn">Top Troops</button>
-            <div class="dropdown-content"><br>
-                
-            <a href="https://alexandregames.com/top-troops/top-troops-en-home.html">Top Troops Guide</a>
-            <a href="https://alexandregames.com/top-troops/top-troops-heroes-guide-en.html">Heroes Guide</a>
-            <a href="https://alexandregames.com/top-troops/top-troops-us-tier-list.html">Class Tier List</a>
-                                    
-                
-                <br><br><br>
-            </div>
-        </div>      
-
         
-        <div class="dropdown">
-            <button><a href="https://alexandregames.com/throne-and-liberty/menu-throne-and-liberty/menu-throne-and-liberty-en.html">Throne and Liberty</a></button>
-        </div>
+        
+        
+        <br><br><br><br>
+        
+    </div>
+</div>  
 
-        <div class="dropdown">
-            <button class="dropbtn">More Games</button>
-            <div class="dropdown-content"><br>
-                <a href="https://alexandregames.com/ragnarok-origin/codes-ragnarok-us.html">Ragnarok ROO</a>
-                <a>_____________</a>
-                <a href="https://alexandregames.com/Documentos/Contact.html">Contact</a>
-                <a href="https://alexandregames.com/Documentos/disclaimer.html">Disclaimer</a>
-                <a href="https://alexandregames.com/Documentos/PrivacyPolicy.html">Privacy Policy</a>
-                <a href="https://alexandregames.com/Documentos/about.html">About Us</a>
-                <a href="https://alexandregames.com/Documentos/TermsofUse.html">Terms of Use</a>
-                <br><br><br>
-            </div>
+<div class="dropdown">
+    <button class="dropbtn">Mobile Legends</button>
+    <div class="dropdown-content"><br>
+        
+    <a href="https://alexandregames.com/MobileLegends-English.html">Heroes Guide</a>
+    <a href="https://alexandregames.com/mobile-legends/article-mlbb/tier-list-mlbb-en.html">MLBB Tier List</a>
+    <a href="https://alexandregames.com/mobile-legends/article-mlbb/gold-lane-guide-en-mlbb.html">Gold Lane Guide</a>
+    <a href="https://alexandregames.com/mobile-legends/article-mlbb/roam-guide-mlbb-en.html">Roam Lane Guide</a>          
+                            
+        
+        <br><br><br>
+    </div>
+</div>      
+
+<div class="dropdown">
+    <button class="dropbtn"><a href="https://alexandregames.com/roblox-us.html">Roblox PC/Mobile</a></button>
+</div> 
+
+<div class="dropdown">
+        <button class="dropbtn">Top Troops</button>
+        <div class="dropdown-content"><br>
+            
+        <a href="https://alexandregames.com/top-troops/top-troops-en-home.html">Top Troops Guide</a>
+        <a href="https://alexandregames.com/top-troops/top-troops-heroes-guide-en.html">Heroes Guide</a>
+        <a href="https://alexandregames.com/top-troops/top-troops-us-tier-list.html">Class Tier List</a>
+                                
+            
+            <br><br><br>
         </div>
-        <div class="dropdown">
-    <button class="language-switch">
-        <img src="https://alexandregames.com/imagens/flag-brazil-32x32.png" alt="Brazil flag" title="Brazil flag" style="width:24px;height:16px">
-        <strong>PT</strong>
-    </button>
+    </div>      
+
+    
+    <div class="dropdown">
+        <button></button>
+    </div>
+
+    <div class="dropdown">
+        <button class="dropbtn">More Games</button>
+        <div class="dropdown-content"><br>
+        <a href="https://alexandregames.com/throne-and-liberty/menu-throne-and-liberty/menu-throne-and-liberty-en.html">Throne and Liberty</a>
+            <a href="https://alexandregames.com/ragnarok-origin/codes-ragnarok-us.html">Ragnarok ROO</a>
+            <a>_____________</a>
+            <a href="https://alexandregames.com/Documentos/Contact.html">Contact</a>
+            <a href="https://alexandregames.com/Documentos/disclaimer.html">Disclaimer</a>
+            <a href="https://alexandregames.com/Documentos/PrivacyPolicy.html">Privacy Policy</a>
+            <a href="https://alexandregames.com/Documentos/about.html">About Us</a>
+            <a href="https://alexandregames.com/Documentos/TermsofUse.html">Terms of Use</a>
+            <br><br><br>
+        </div>
+    </div>
+    <div class="dropdown">
+<button class="language-switch">
+    <img src="https://alexandregames.com/imagens/flag-brazil-32x32.png" alt="Brazil flag" title="Brazil flag" style="width:24px;height:16px">
+    <strong>PT</strong>
+</button>
 </div>
     </ul>
     <div class="menu-icon">
--- Documentos/nav13-pt-v2.js
+++ Documentos/nav13-pt-v2.js
@@ -1,108 +1,122 @@
  // atualizar somente os nav13-en-v2.js e nav13-pt-v2.js esses são com a bandeira no nav
 document.addEventListener('DOMContentLoaded', () => {
     const navHTML = `
-        <div class="logo">
-            <a href="https://alexandregames.com/index-pt.html"><img src="https://alexandregames.com/imagens/exandregames318x318-removebg.webp" alt="logo Alexandre Games Mobile" title="logo Alexandre Games Mobile"></a>
-        </div>                     
-        <ul>             
-        
-        <div class="dropdown">
-            <button class="dropbtn">Análises de Gameplay</button>
-            <div class="dropdown-content"><br>
-                
-            <a href="https://alexandregames.com/gaming-guide/review/appgallery-review-pt.html">Instale o AppGallery no Android para descontos em Hero Wars</a>
-            <a href="https://alexandregames.com/gaming-guide/review/echoes-of-eternity-game-review-pt.html">Echoes of Eternity</a>
-            <a href="https://alexandregames.com/hero-wars-alliance/guide/how-to-play-hero-wars-on-pc-and-mac-pt.html">Jogue Hero Wars Alliance no PC e Mac</a>
-             <a href="https://alexandregames.com/gaming-guide/review/tales-of-wind-review-pt.html">Tales of Wind: RR</a>
-            <a href="https://alexandregames.com/gaming-guide/review/the-dragon-odyssey-gameplay-pt.html">The Dragon Odyssey</a>
-                                    
-                
-                <br><br><br>
-                  <br><br><br>
-                 
-            </div>
-        </div>    
-
-
-            <div class="dropdown">
-                <button class="dropbtn">Hero Wars Alliance</button>
-                <div class="dropdown-content"><br>
-                 
-                <a href="https://alexandregames.com/HeroWars.html">HWA - Início</a>  
-                <a href="https://alexandregames.com/hero-wars-alliance/menu-review-pt-hwa.html">Análises de Skins</a>  
-                <a href="https://alexandregames.com/Hero%20Wars/calendar-event-pt-hwa.html">Calendário & Eventos</a>
-                <a href="https://alexandregames.com/hero-wars-alliance/event-hwa/herosway-pt-hwa.html">Caminho do Herói</a> 
-                <a href="https://alexandregames.com/Hero%20Wars/Guiadeherois.html">Guia de Heróis</a>   
-                <a href="https://alexandregames.com/hero-wars-alliance/menu-best-teams-pt-hwa.html">Guia de Melhores Times</a> 
-                <a href="https://alexandregames.com/Hero%20Wars/Guia%20de%20Titans.html">Guia de Titãs</a>                                 
-                <a href="https://alexandregames.com/hero-wars-alliance/menu-tactics-secrets-hwa-en.html">Táticas & Segredos</a> 
-                <a href="https://alexandregames.com/Hero%20Wars/articles-herowars/tierlist-pt-pvp-herowars.html">Tier List</a> 
-                
-                
-                    
-                   <br><br><br><br>
-                 
-                </div>  
-            </div>   
-            
-            <div class="dropdown">
-                <button class="dropbtn">Mobile Legends</button>
-                <div class="dropdown-content"><br>
-                 
-            <a href="https://alexandregames.com/MobileLegends.html">Guia de Heróis</a>
-            <a href="https://alexandregames.com/mobile-legends/article-mlbb/tier-list-mlbb-pt.html">MLBB Tier List</a>
-            <a href="https://alexandregames.com/mobile-legends/article-mlbb/gold-lane-guide-pt-mlbb.html">Guia da Rota do Ouro</a>
-            <a href="https://alexandregames.com/mobile-legends/article-mlbb/roam-guide-mlbb-pt.html">Guia da Rotação</a>                              
-                    
-                   <br><br><br> <br><br><br>
-                </div>
-            </div>      
-
-            
-            <div class="dropdown">
-                <button class="dropbtn"><a href="https://alexandregames.com/roblox-br.html">Roblox PC/Mobile</a></button>
-                
-            </div> 
+<div class="logo">
+    <a href="https://alexandregames.com/index-pt.html"><img src="https://alexandregames.com/imagens/exandregames318x318-removebg.webp" alt="logo Alexandre Games Mobile" title="logo Alexandre Games Mobile"></a>
+</div>                     
+<ul>             
 
 <div class="dropdown">
-            <button class="dropbtn">Top Troops</button>
-            <div class="dropdown-content"><br>
-                
-            <a href="https://alexandregames.com/top-troops/top-troops-pt-home.html">Guia de Top Troops</a>
-            <a href="https://alexandregames.com/top-troops/top-troops-heroes-guide-pt.html">Guia de Heróis</a>
-            <a href="https://alexandregames.com/top-troops/top-troops-pt-tier-list.html">Tier List de Classe</a>
-                                    
-                
-                <br><br><br>
-            </div>
-        </div>      
-
-             <div class="dropdown">
-               <button><a href="https://alexandregames.com/throne-and-liberty/menu-throne-and-liberty/menu-throne-and-liberty-pt.html">Throne and Liberty</a></button>
-            </div>
-            <div class="dropdown">
-                <button class="dropbtn">Mais Jogos</button>
-                <div class="dropdown-content"><br>
-                    <a href="https://alexandregames.com/ragnarok-origin/codigos-ragnarok-br.html">Ragnarok ROO</a>
-                    <a>_____________</a>
-                    <a href="https://alexandregames.com/Documentos/Contato.html">Contato</a>
-                    <a href="https://alexandregames.com/Documentos/aviso-legal.html">Isenção de Responsabilidade</a>
-                    <a href="https://alexandregames.com/Documentos/Pol%C3%ADtica%20de%20privacidade.html">Política de Privacidade</a>
-                    <a href="https://alexandregames.com/Documentos/Sobre.html">Sobre Nós</a>
-                    <a href="https://alexandregames.com/Documentos/Termos%20de%20uso.html">Termos de Uso</a>
-                    <br><br><br>
-                </div>
-            </div>
-        <div class="dropdown">
-        <button class="language-switch">
-            <img src="https://alexandregames.com/imagens/flag-us-32x32.png" alt="bandeira USA" title="bandeira" USA style="width:24px;height:16px" >
-            <strong>EN</strong>
-        </button>
+    <button class="dropbtn">Análises de Gameplay</button>
+    <div class="dropdown-content"><br>
+        
+    <a href="https://alexandregames.com/gaming-guide/review/appgallery-review-pt.html">Instale o AppGallery no Android para descontos em Hero Wars</a>
+    <a href="https://alexandregames.com/gaming-guide/review/echoes-of-eternity-game-review-pt.html">Echoes of Eternity</a>
+    <a href="https://alexandregames.com/hero-wars-alliance/guide/how-to-play-hero-wars-on-pc-and-mac-pt.html">Jogue Hero Wars Alliance no PC e Mac</a>
+        <a href="https://alexandregames.com/gaming-guide/review/tales-of-wind-review-pt.html">Tales of Wind: RR</a>
+    <a href="https://alexandregames.com/gaming-guide/review/the-dragon-odyssey-gameplay-pt.html">The Dragon Odyssey</a>
+                            
+        
+        <br><br><br>
+            <br><br><br>
+            
     </div>
-        </ul>
-        <div class="menu-icon">
-            <img src="https://alexandregames.com/imagens/menu.png" alt="Menu" style="width:48px;height:48px;">
+</div>    
+
+
+<div class="dropdown">
+    <button class="dropbtn">Hero Wars Alliance</button>
+    <div class="dropdown-content"><br>
+        
+    <a href="https://alexandregames.com/HeroWars.html">HWA - Início</a>  
+    <a href="https://alexandregames.com/hero-wars-alliance/menu-review-pt-hwa.html">Análises de Skins</a>  
+    <a href="https://alexandregames.com/Hero%20Wars/calendar-event-pt-hwa.html">Calendário & Eventos</a>
+    <a href="https://alexandregames.com/hero-wars-alliance/event-hwa/herosway-pt-hwa.html">Caminho do Herói</a> 
+    <a href="https://alexandregames.com/Hero%20Wars/Guiadeherois.html">Guia de Heróis</a>   
+    <a href="https://alexandregames.com/hero-wars-alliance/menu-best-teams-pt-hwa.html">Guia de Melhores Times</a> 
+    <a href="https://alexandregames.com/Hero%20Wars/Guia%20de%20Titans.html">Guia de Titãs</a>                                 
+    <a href="https://alexandregames.com/hero-wars-alliance/menu-tactics-secrets-hwa-en.html">Táticas & Segredos</a> 
+    <a href="https://alexandregames.com/Hero%20Wars/articles-herowars/tierlist-pt-pvp-herowars.html">Tier List</a> 
+    
+    
+        
+        <br><br><br><br>
+        
+    </div>  
+</div>   
+
+    <div class="dropdown">
+
+<button class="dropbtn">Hero Wars: Dominion Era</button>
+<div class="dropdown-content"> <br>  
+            
+<a href="https://alexandregames.com/hero-wars-dominion-era/guide/totems-guide-pt.html">Guia de Totens</a>
+<a href="https://alexandregames.com/hero-wars-dominion-era/guide/pets-tier-list-pt.html">Tier List de PET</a>
+
+
+
+
+<br><br><br><br>
+
+</div>
+</div>  
+    
+    <div class="dropdown">
+        <button class="dropbtn">Mobile Legends</button>
+        <div class="dropdown-content"><br>
+            
+    <a href="https://alexandregames.com/MobileLegends.html">Guia de Heróis</a>
+    <a href="https://alexandregames.com/mobile-legends/article-mlbb/tier-list-mlbb-pt.html">MLBB Tier List</a>
+    <a href="https://alexandregames.com/mobile-legends/article-mlbb/gold-lane-guide-pt-mlbb.html">Guia da Rota do Ouro</a>
+    <a href="https://alexandregames.com/mobile-legends/article-mlbb/roam-guide-mlbb-pt.html">Guia da Rotação</a>                              
+            
+            <br><br><br> <br><br><br>
         </div>
+    </div>      
+
+    
+    <div class="dropdown">
+        <button class="dropbtn"><a href="https://alexandregames.com/roblox-br.html">Roblox PC/Mobile</a></button>
+        
+    </div> 
+
+<div class="dropdown">
+    <button class="dropbtn">Top Troops</button>
+    <div class="dropdown-content"><br>
+        
+    <a href="https://alexandregames.com/top-troops/top-troops-pt-home.html">Guia de Top Troops</a>
+    <a href="https://alexandregames.com/top-troops/top-troops-heroes-guide-pt.html">Guia de Heróis</a>
+    <a href="https://alexandregames.com/top-troops/top-troops-pt-tier-list.html">Tier List de Classe</a>
+                            
+        
+        <br><br><br>
+    </div>
+</div>      
+
+<div class="dropdown">
+<button class="dropbtn">Mais Jogos</button>
+<div class="dropdown-content"><br>
+    <a href="https://alexandregames.com/throne-and-liberty/menu-throne-and-liberty/menu-throne-and-liberty-pt.html">Throne and Liberty</a>
+    <a href="https://alexandregames.com/ragnarok-origin/codigos-ragnarok-br.html">Ragnarok ROO</a>
+    <a>_____________</a>
+    <a href="https://alexandregames.com/Documentos/Contato.html">Contato</a>
+    <a href="https://alexandregames.com/Documentos/aviso-legal.html">Isenção de Responsabilidade</a>
+    <a href="https://alexandregames.com/Documentos/Pol%C3%ADtica%20de%20privacidade.html">Política de Privacidade</a>
+    <a href="https://alexandregames.com/Documentos/Sobre.html">Sobre Nós</a>
+    <a href="https://alexandregames.com/Documentos/Termos%20de%20uso.html">Termos de Uso</a>
+    <br><br><br>
+</div>
+</div>
+<div class="dropdown">
+<button class="language-switch">
+    <img src="https://alexandregames.com/imagens/flag-us-32x32.png" alt="bandeira USA" title="bandeira" USA style="width:24px;height:16px" >
+    <strong>EN</strong>
+</button>
+</div>
+</ul>
+<div class="menu-icon">
+    <img src="https://alexandregames.com/imagens/menu.png" alt="Menu" style="width:48px;height:48px;">
+</div>
     `;
 
     const navElement = document.createElement('nav');
