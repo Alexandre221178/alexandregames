70f378220905de7cafd883566f48ce4983a98a7d
Alexandre221178 <alexandredomingos2211@gmail.com>
2026-06-14 15:11:56 -0300

    fix

--- hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
+++ hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
@@ -11,9 +11,11 @@
   // ── 2 Link Exclusivo Web/FB ──────────────────────────────
 { date:'2026-0#-##30', type:'link', giftLinkWeb:'https://herowars.me/##', giftLinkFb:'https://herowars.me/##', note:'until|2026-05-03T02:00:00Z',
   rewards:['energy x500'] },
-https://www.hero-wars.com/?nx_source=group_posting_boxweb&gift_id=QFfxfSq5yk
+https://www.hero-wars.com/?nx_source=group_posting_boxweb&gift_id=51a884bf9fd9479aff006545b4db39d5
 */window.HWDE_GIFTS = [
    // ── June 14, 2026 ──────────────────────────────
+   { date:'2026-06-14', type:'link', giftId:'51a884bf9fd9479aff006545b4db39d5', note:'level',
+  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
 { date:'2026-06-14', type:'link', giftId:'QFfxfSq5yk', note:'until|2026-06-17T02:00:00Z',
   rewards:['energy x500'] },
    // ── June 13, 2026 ──────────────────────────────
@@ -24,187 +26,25 @@ https://www.hero-wars.com/?nx_source=group_posting_boxweb&gift_id=QFfxfSq5yk
   { date:'2026-06-12', type:'link', giftLinkWeb:'https://herowars.me/4eftaX1', giftLinkFb:'https://herowars.me/3QCQnKW ', note:'until|2026-06-16T02:00:00Z',
   rewards:['energy x500'] },
    // ── June #, 2026 ──────────────────────────────
-    { date:'2026-06-11', type:'link', giftId:'2f39e8f1e7f0bebb883630985f09e828', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
+  
  { date:'2026-06-11', type:'link', giftLinkWeb:'https://herowars.me/4xCtuZ1', giftLinkFb:'https://herowars.me/3PRrPxF', note:'until|2026-06-14T02:00:00Z',
   rewards:['energy x500'] },
    // ── June 10, 2026 ──────────────────────────────
-   { date:'2026-06-10', type:'link', giftId:'811d37497aa96f10f1fe566529f9cff8', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
+  
   { date:'2026-06-10', type:'link', giftLinkWeb:'https://herowars.me/3Qi2VqS', giftLinkFb:'https://herowars.me/4ediGHL', note:'until|2026-06-12T02:00:00Z',
   rewards:['energy x500'] },
    // ── June 9, 2026 ──────────────────────────────
-   { date:'2026-06-09', type:'link', giftId:'24317e4768e67acdb887dc296375a6ab', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
+  
 // ── June 8, 2026 ──────────────────────────────
-   { date:'2026-06-08', type:'link', giftId:'91847deed0acc4c834df62c57180e49f', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
+  
      { date:'2026-06-08', type:'link', giftLinkWeb:'https://herowars.me/49JGpxP', giftLinkFb:'https://herowars.me/4xdHcB9', note:'until|2026-06-12T02:00:00Z',
   rewards:['energy x500'] },
-   // ── June 7, 2026 ──────────────────────────────
-  { date:'2026-06-07', type:'link', giftId:'c70fff056f45dbd589917dced25d41a5', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
+   
    // ── June #, 2026 ──────────────────────────────
-   { date:'2026-06-06', type:'link', giftId:'f3ef2f11cb24f27e3bc59d3c06d38d77', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
-   { date:'2026-06-06', type:'link', giftLinkWeb:'https://herowars.me/4vmBn2s', giftLinkFb:'https://herowars.me/4vuRICf', note:'until|2026-06-103T02:00:00Z',
+     { date:'2026-06-06', type:'link', giftLinkWeb:'https://herowars.me/4vmBn2s', giftLinkFb:'https://herowars.me/4vuRICf', note:'until|2026-06-10T02:00:00Z',
   rewards:['energy x500'] },
-    { date:'2026-06-05', type:'link', giftId:'3bf77b89604b4f19c417e641eee30bdf', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-   // ── June 5, 2026 ──────────────────────────────
-{ date:'2026-06-05', type:'link', giftId:'Js6SrG1VjG', note:'until|2026-06-06T02:00:00Z',
-  rewards:['energy x500'] },
-   // ── June 4, 2026 ──────────────────────────────
-   { date:'2026-06-04', type:'link', giftId:'ecb6c7cd91a21228ae413b8391c789f8', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-   // ── June 3, 2026 ──────────────────────────────
-   { date:'2026-06-03', type:'link', giftId:'8bd9f6fa50b731b2052c9524b9ac440e', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },   
-   // ── June 3, 2026 ──────────────────────────────
-{ date:'2026-06-03', type:'link', giftId:'NPzrOnPpZk', note:'until|2026-06-06T02:00:00Z',
-  rewards:['energy x500'] },
-   // ── June 2, 2026 ──────────────────────────────
-   { date:'2026-06-02', type:'link', giftId:'aeb77b8c1b1ef9e7f3a415f68849775e', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-   // ── June 1, 2026 ──────────────────────────────
-     { date:'2026-06-01', type:'link', giftId:'ea1041fa548f16a3816fda9150d73edf', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-{ date:'2026-06-01', type:'link', giftId:'FNswcTxPet', note:'until|2026-06-06T02:00:00Z',
-  rewards:['energy x500'] },
-   // ── May 31, 2026 ──────────────────────────────
-  { date:'2026-05-31', type:'link', giftId:'0195fcabfe3a6e55b16656499d332075', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
-   // ── May 30, 2026 ──────────────────────────────
-    { date:'2026-05-30', type:'link', giftId:'3719287519a1b042d72f04e6fdbb241b', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
-{ date:'2026-05-30', type:'link', giftId:'zZR9zxmoa2', note:'until|2026-06-02T02:00:00Z',
-  rewards:['energy x500'] },
- 
-      // ── May 29, 2026 ──────────────────────────────
-   // ── 1 Link Exclusivo so WebId diferente de weblink ──────────────────────────────
-{ date:'2026-05-29', type:'link', giftWebId:'dUPLoCFwC8', note:'until|2026-05-31T02:00:00Z',
-  rewards:['valor-coin x10000'] },
-   { date:'2026-05-29', type:'link', giftId:'55bb4d58d9a1479ab274b6bf0ef3f1ea', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-   // ── May 28, 2026 ──────────────────────────────
-   { date:'2026-05-28', type:'link', giftId:'6c4bb1271863d3e010dfb853e575b114', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-   // ── 2 Link Exclusivo Web/FB ──────────────────────────────
-{ date:'2026-05-28', type:'link', giftLinkWeb:'https://herowars.me/4dzyWnn', giftLinkFb:'https://herowars.me/4u007wo', note:'until|2026-05-31T02:00:00Z',
-    rewards:['energy-crystal x16', 'valor-coin x8500', 'sapphire-medallion x1'] },
-{ date:'2026-05-28', type:'link', giftId:'tfI6otBkHP', note:'until|2026-05-31T02:00:00Z',
-  rewards:['energy x500'] },
-   // ── May 27, 2026 ──────────────────────────────
-   { date:'2026-05-27', type:'link', giftId:'ffe723e6e637780033588bc9f1c79a53', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-    // ── 2 Link Exclusivo Web/FB ──────────────────────────────
-{ date:'2026-05-25', type:'link', giftLinkWeb:'https://herowars.me/4e6cpP4', giftLinkFb:'https://herowars.me/3PNqQOS', note:'until|2026-05-31T02:00:00Z',
-    rewards:['energy-crystal x16', 'valor-coin x8000', 'sapphire-medallion x2'] },
-
-{ date:'2026-05-27', type:'link', giftId:'ad5y4SSYWn', note:'until|2026-05-31T02:00:00Z',
-  rewards:['energy x500'] },
-   { date:'2026-05-26', type:'link', giftId:'544a093be3c3fea36924dd9eb24abc2b', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-   // ── May 25, 2026 ──────────────────────────────
-   { date:'2026-05-25', type:'link', giftId:'a19e114cb11e9e5b59afdc7ebc8671dd', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-   // ── 2 Link Exclusivo Web/FB ──────────────────────────────
-{ date:'2026-05-25', type:'link', giftLinkWeb:'https://herowars.me/4v2FhNV', giftLinkFb:'https://herowars.me/4ds0P0F', note:'until|2026-05-28T02:00:00Z',
-  rewards:['energy x500'] },
-
-  // ── May 25, 2026 ──────────────────────────────
-{ date:'2026-05-25', type:'link', giftId:'TQxDlP0cWr', note:'until|2026-05-28T02:00:00Z',
-  rewards:['energy x500'] },
-  // ── May 24, 2026 ──────────────────────────────
-  { date:'2026-05-24', type:'link', giftId:'fea9ae295b4f907972ce8548fa590b3b', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
-
-  // ── Gifts New hero or titan ──────────────────────────────
- { date:'2026-05-24', type:'link', giftId:'vKDpFTe82s', note:'until|2026-05-31T02:00:00Z',
-  rewards:['energy-crystal x16', 'valor-coin x8500', 'sapphire-medallion x2'] },
-  // ── May 23, 2026 ──────────────────────────────
-  { date:'2026-05-23', type:'link', giftId:'f2a458703f59077118711c2579bf2726', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
-
-   // ── May 22, 2026 ──────────────────────────────
-   { date:'2026-05-22', type:'link', giftId:'86a2c1e47a06a0143524fff8d8cd5cb1', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-{ date:'2026-05-22', type:'link', giftId:'yYlpsgzY7H', note:'until|2026-05-26T02:00:00Z',
-  rewards:['energy x500'] },
-   // ── May 21, 2026 ──────────────────────────────
-   { date:'2026-05-21', type:'link', giftId:'041586f8d10f2d9f0f9d78ef0b881221', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-   // ── May 20, 2026 ──────────────────────────────
-   { date:'2026-05-20', type:'link', giftId:'fd75b7e424d4d976537a3cea2a946a5e', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-   // ── May 19, 2026 ──────────────────────────────
-   { date:'2026-05-19', type:'link', giftId:'bb63a39c82ae8edcac9504cf8b3be309', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
+   
     
-  // ── May 18, 2026 ──────────────────────────────
-   { date:'2026-05-18', type:'link', giftId:'a6b403ceebca65436104cf6b9646728e', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-{ date:'2026-05-18', type:'link', giftId:'uuFCjoN8Sw', note:'until|2026-05-21T02:00:00Z',
-  rewards:['energy x500'] },
-   { date:'2026-05-17', type:'link', giftId:'cfa837ff19cb7a5b8ea906954b65d786', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
-  { date:'2026-05-17', type:'link', giftId:'XZmw4ci3Pd', note:'until|2026-05-22T02:00:00Z',
-  rewards:['energy x500'] },
-  // ── May #, 2026 ──────────────────────────────
-  { date:'2026-05-16', type:'link', giftId:'357f22bc0768f99308ad834a6aac7316', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
-   // ── May 15, 2026 ──────────────────────────────
-   { date:'2026-05-15', type:'link', giftId:'4a766740d74a156ee227eee68971f219', note:'level',
-   rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-   // ── 2 Link Exclusivo Web/FB postado atrasado ──────────────────────────────
-{ date:'2026-05-14', type:'link', giftLinkWeb:'https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa3BlbXdoZ2Z3UmhaVTJTMjJURzk4ZHVJeHQ3QXxBQ3Jtc0tsQnBjUGwzOE5Cc044c0lFWXp3a2JlWk5MbGFQR3RaZXdwa3dCUWpPYzlMcWo3S1EzY1hZaGtQQXJIN014bzY4akxBSEx5YjNvSDBfdmhUamhzdFZ5OXRZdWduYnVVWW9ZNjUwU0c3YUgtSk13UjQ1bw&q=https%3A%2F%2Fwww.hero-wars.com%2F%3Fnx_source%3Dgroup_posting_boxweb%26gift_id%3D8PdgKQBHWP&v=6ICF5LDfVTo',
-   giftLinkFb:'https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbl9yMHZwSGhzWDRJcmJqajlEZTlHSU5nMy1RUXxBQ3Jtc0trSXdxaUNDb1pEOGFkUzMxOTFaV0MyUHFZckZoaVZSSFFuUnVSLVhGS3hlUVFPUGhhOEtfNkFYYVBFRndDeGk3elhtZGZkMTRMczc1QWtKcDlRaGN2eW0yMXktXzVjQ25UeVFpU21DNVp6WnVLX2pnbw&q=https%3A%2F%2Ffb.gg%2Fplay%2Fmobaheroes%2Fd%2F4546179685671400%2F&v=6ICF5LDfVTo', note:'until|2026-05-03T02:00:00Z',
-  note:'until|2026-05-19T02:00:00Z',
-  rewards:['large-skin-stone-chest x5', 'portal-charge x3'] },
-  // ── May 14, 2026 ──────────────────────────────
-   { date:'2026-05-14', type:'link', giftId:'735837ad037b8cdec5631388bb31bc3e', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-{ date:'2026-05-14', type:'link', giftId:'e0Gc5jwrK4', note:'until|2026-05-17T02:00:00Z',
-  rewards:['energy x500'] },
-  // ── May 13, 2026 ──────────────────────────────
-    { date:'2026-05-13', type:'link', giftId:'10f2080e4235a827cf17c02fba7984b3', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-{ date:'2026-05-13', type:'link', giftId:'SkqbwOjufI', note:'until|2026-05-16T02:00:00Z',
-  rewards:['energy x500'] },
-   // ── May 12, 2026 ──────────────────────────────
-   { date:'2026-05-12', type:'link', giftId:'17065276b57d0655a234f1873235f2c6', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-   // ── May #, 2026 ──────────────────────────────
-   { date:'2026-05-11', type:'link', giftId:'617a2567d140714a8cb0f2aa09e97cb8', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-   
-  { date:'2026-05-10', type:'link', giftId:'7919fa2d4bf090331a1c6efc67333024', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
-  { date:'2026-05-10', type:'link', giftId:'leWjCcMyjS', note:'until|2026-05-14T02:00:00Z',
-  rewards:['energy x500'] },
-  // ── May 9, 2026 ──────────────────────────────
-  { date:'2026-05-09', type:'link', giftId:'5d1286dc3bf024661d85bee6224183c5', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
-  { date:'2026-05-09', type:'link', giftId:'pys2qu5thj', note:'until|2026-05-14T02:00:00Z',
-  rewards:['energy-crystal x24', 'valor-coin x12500', 'sapphire-medallion x3'] },
-
-{ date:'2026-05-09', type:'link', giftId:'fvynEMCqac', note:'until|2026-05-12T02:00:00Z',
-  rewards:['energy x500'] },
-
-   // ── May 8, 2026 ──────────────────────────────
-   { date:'2026-05-08', type:'link', giftId:'7bcf526ede49a8eafb48c7da0d3f7913', note:'level',
-  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
-// ── May 7, 2026 ──────────────────────────────
-   
-{ date:'2026-05-07', type:'link', giftId:'3WUHe5nf44', note:'until|2026-05-14T02:00:00Z',
-  rewards:['energy-crystal x24', 'valor-coin x12500', 'sapphire-medallion x2'] },
-
-   
-  // ── May 5, 2026 ──────────────────────────────
- 
-{ date:'2026-05-05', type:'link', giftId:'uZHHmfMazu', note:'until|2026-05-09T02:00:00Z',
-  rewards:['energy x500'] },
-  
   
   
 
@@ -248,7 +88,11 @@ link type  : { date:'YYYY-MM-DD', type:'link',  giftId:'...', note:'until|YYYY-M
 { date:'2026-03-27', type:'link', giftId:'xqYzCnjZ49', note:'until|2026-04-05T02:00:00Z',
   rewards:['large-skin-stone-chest x10', 'friendship-chip x10000', 'gold 1M'] },
 
-
+// ── 2 Link Exclusivo Web/FB postado atrasado ──────────────────────────────
+{ date:'2026-05-14', type:'link', giftLinkWeb:'https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa3BlbXdoZ2Z3UmhaVTJTMjJURzk4ZHVJeHQ3QXxBQ3Jtc0tsQnBjUGwzOE5Cc044c0lFWXp3a2JlWk5MbGFQR3RaZXdwa3dCUWpPYzlMcWo3S1EzY1hZaGtQQXJIN014bzY4akxBSEx5YjNvSDBfdmhUamhzdFZ5OXRZdWduYnVVWW9ZNjUwU0c3YUgtSk13UjQ1bw&q=https%3A%2F%2Fwww.hero-wars.com%2F%3Fnx_source%3Dgroup_posting_boxweb%26gift_id%3D8PdgKQBHWP&v=6ICF5LDfVTo',
+   giftLinkFb:'https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbl9yMHZwSGhzWDRJcmJqajlEZTlHSU5nMy1RUXxBQ3Jtc0trSXdxaUNDb1pEOGFkUzMxOTFaV0MyUHFZckZoaVZSSFFuUnVSLVhGS3hlUVFPUGhhOEtfNkFYYVBFRndDeGk3elhtZGZkMTRMczc1QWtKcDlRaGN2eW0yMXktXzVjQ25UeVFpU21DNVp6WnVLX2pnbw&q=https%3A%2F%2Ffb.gg%2Fplay%2Fmobaheroes%2Fd%2F4546179685671400%2F&v=6ICF5LDfVTo', note:'until|2026-05-03T02:00:00Z',
+  note:'until|2026-05-19T02:00:00Z', rewards:['large-skin-stone-chest x5', 'portal-charge x3']
+ },
 
 ╔══════════════════════════════════════════════════════════════════╗
    ║  HWDE GIFTS — Add newest entries at the TOP                     ║
