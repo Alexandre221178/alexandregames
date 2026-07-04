234dc1d5f68dbf3cb1bbd39f3ff0d6d796bccc92
Alexandre221178 <alexandredomingos2211@gmail.com>
2026-07-04 15:20:59 -0300

    ok

--- hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
+++ hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
@@ -11,8 +11,11 @@
   // 2026-07-##── 2 Link Exclusivo Web/FB ──────────────────────────────
 { date:'2026-07-##', type:'link', giftLinkWeb:'https://herowars.me/#wb#', giftLinkFb:'https://herowars.me/#fb#',
  note:'until|2026-07-##T02:00:00Z', rewards:['energy x500'] },
-
+https://www.hero-wars.com/?nx_source=group_posting_boxweb&gift_id=
 */window.HWDE_GIFTS = [
+  // ── July 4, 2026 ──────────────────────────────
+  { date:'2026-07-04', type:'link', giftId:'478715ede764934568a6b4437c05a1b7', note:'level',
+  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
    // 2026-07-04── 2 Link Exclusivo Web/FB ──────────────────────────────
 { date:'2026-07-04', type:'link', giftLinkWeb:'https://herowars.me/4vafW4o', giftLinkFb:'https://herowars.me/4yb6Ua7',
  note:'until|2026-07-07T02:00:00Z', rewards:['energy x500', 'violet-equipment-fragment-box-mage x1',  'violet-equipment-fragment-box-tank x1', 'violet-equipment-fragment-box-healer x1'] },
