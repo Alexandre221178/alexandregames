591cda67f9f33b3a01600d681888e6ebe32ed417
Alexandre221178 <alexandredomingos2211@gmail.com>
2026-07-07 19:30:50 -0300

    see

--- hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
+++ hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
@@ -18,6 +18,9 @@ https://www.hero-wars.com/?nx_source=group_posting_boxweb&gift_id=
   // 2026-07-07── 2 Link Exclusivo Web/FB ──────────────────────────────
 { date:'2026-07-07', type:'link', giftLinkWeb:'https://herowars.me/4w8abFP', giftLinkFb:'https://herowars.me/4w8d2yx',
  note:'until|2026-07-07T02:00:00Z', rewards:['energy x500'] },
+ // ── 1 Link Exclusivo so giftWebId diferente de weblink esse so coloca o id ──────────────────────────────
+{ date:'2026-07-06', type:'link', giftWebId:'ZryXlzQxy2', note:'until|2026-05-31T02:00:00Z',
+  rewards:['honey-energy x2000'] },
    // ── July 06, 2026 ──────────────────────────────
    { date:'2026-07-06', type:'link', giftId:'db6a34d21c88ec45d81a1f2ae36ecdbb', note:'level',
   rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
@@ -192,7 +195,7 @@ https://www.hero-wars.com/?nx_source=group_posting_boxweb&gift_id=
   rewards:['energy x500'] },
 
   // ── 1 Link Exclusivo so giftWebId diferente de weblink esse so coloca o id ──────────────────────────────
-{ date:'2026-05-29', type:'link', giftWebId:'dUPLoCFwC8', note:'until|2026-05-31T02:00:00Z',
+{ date:'2026-##-##', type:'link', giftWebId:'#####', note:'until|2026-05-31T02:00:00Z',
   rewards:['valor-coin x1000'] },
 
   // ── Gifts New hero or titan ──────────────────────────────
