2fff2a84c7b96090fd8381edbf889c36b52685be
Alexandre221178 <133829010+Alexandre221178@users.noreply.github.com>
2026-07-29 13:34:04 -0300

    Add new hub gift entry for July 29, 2026

--- hero-wars-alliance/ag-daily-gifts-hwa/gifts-hwa-data.js
+++ hero-wars-alliance/ag-daily-gifts-hwa/gifts-hwa-data.js
@@ -4,6 +4,12 @@
 rewards:['silver-casket x1', 'energy x20', 'realm-casket x1'] },
 */
 window.HWA_GIFTS = [
+   // ── Hub Code nao definido a data false ──────────────────────────────
+{ date:'2026-07-29', type:'hub', code:'PALLANT', note:'custom',
+    customNote:{ en:'The gift is available until... not defined(or 24hs)', pt:'O presente est\u00e1 dispon\u00edvel at\u00e9... n\u00e3o definido(expirado ou 24h)', de:'Das Geschenk ist verf\u00fcgbar bis... nicht definiert(abgelaufen oder 24h)', es:'El regalo est\u00e1 disponible hasta... no definido(expirado o 24h)', fr:'Le cadeau est disponible jusqu\u0027\u00e0... non d\u00e9fini(expir\u00e9 ou 24h)', ja:'\u30ae\u30d5\u30c8\u306e\u6709\u52b9\u671f\u9650... \u672a\u5b9a\u7fa9\uff08\u671f\u9650\u5207\u308c\u307e\u305f\u306f24\u6642\u9593\uff09' },
+    rewards:['energy x20', 'rune-stone x1500', 'artifact-chest-key x5'] },
+
+   
    // ── July 29, 2026 ──────────────────────────────
    { date:'2026-07-29', type:'daily', mission:'2-3', note:'daily|80',
 rewards:['silver-casket x1', 'energy x20', 'realm-casket x1'] },
