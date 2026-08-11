dc553a7f722dfdca07966315b7058d8c9a5689ea
Alexandre221178 <alexandredomingos2211@gmail.com>
2026-08-11 06:44:30 -0300

    ok

--- hero-wars-alliance/ag-daily-gifts-hwa/gifts-hwa-data.js
+++ hero-wars-alliance/ag-daily-gifts-hwa/gifts-hwa-data.js
@@ -4,6 +4,8 @@
     rewards:['silver-casket x1', 'energy x20', 'realm-casket x1'] },
 */
 window.HWA_GIFTS = [
+  { date:'2026-08-11', type:'daily', mission:'3-4', note:'daily|80',
+    rewards:['silver-casket x1', 'energy x20', 'realm-casket x1'] },
   { date:'2026-08-10', type:'daily', mission:'2-3', note:'daily|80',
     rewards:['silver-casket x1', 'energy x20', 'realm-casket x1'] },
 
