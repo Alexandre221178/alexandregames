1ae56930692bc05496c5cf8fee382c1000d11300
Alexandre221178 <alexandredomingos2211@gmail.com>
2026-08-12 19:08:45 -0300

    esp. g

--- hero-wars-alliance/ag-daily-gifts-hwa/gifts-hwa-data.js
+++ hero-wars-alliance/ag-daily-gifts-hwa/gifts-hwa-data.js
@@ -4,6 +4,10 @@
     rewards:['silver-casket x1', 'energy x20', 'realm-casket x1'] },
 */
 window.HWA_GIFTS = [
+  { date:'2026-08-12', type:'hub', code:'GETSTARTED', note:'custom',
+    customNote:{ en:'The gift is available until... not defined(or 24hs)', pt:'O presente est\u00e1 dispon\u00edvel at\u00e9... n\u00e3o definido(expirado ou 24h)', de:'Das Geschenk ist verf\u00fcgbar bis... nicht definiert(abgelaufen oder 24h)', es:'El regalo est\u00e1 disponible hasta... no definido(expirado o 24h)', fr:'Le cadeau est disponible jusqu\u0027\u00e0... non d\u00e9fini(expir\u00e9 ou 24h)', ja:'\u30ae\u30d5\u30c8\u306e\u6709\u52b9\u671f\u9650... \u672a\u5b9a\u7fa9\uff08\u671f\u9650\u5207\u308c\u307e\u305f\u306f24\u6642\u9593\uff09' },
+  rewards:['energy x150', 'large-skin-stone-chest x3', 'gold 100k', 'big-exp-potion x5' ] },
+
   { date:'2026-08-12', type:'daily', mission:'2-11', note:'daily|80',
     rewards:['silver-casket x1', 'energy x20', 'realm-casket x1'] },
     { date:'2026-08-11', type:'special', mission:'2-14', note:'until|2026-08-31T01:00:00Z',
