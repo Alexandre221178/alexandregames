f6bd5af1cd99023e3d19247e7b3c3f0c13e700ec
Alexandre221178 <alexandredomingos2211@gmail.com>
2026-08-16 08:15:35 -0300

    gifits

--- hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
+++ hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
@@ -11,6 +11,9 @@ https://www.hero-wars.com/?nx_source=group_posting_boxweb&gift_id=
  note:'until|2026-08-##T02:00:00Z', rewards:['energy x500'] },
 https://www.hero-wars.com/?nx_source=group_posting_boxweb&gift_id=
 */window.HWDE_GIFTS = [
+  // 2026-08-16── 2 Link Exclusivo Web/FB ──────────────────────────────
+{ date:'2026-08-16', type:'link', giftLinkWeb:'https://herowars.me/4cjSYkA', giftLinkFb:'https://herowars.me/4zmr9SQ',
+ note:'until|2026-08-20T02:00:00Z', rewards:['energy x500'] },
    { date:'2026-08-15', type:'link', giftId:'e158f04c0542b7f1a71cda25267bfe83', note:'level',
   rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'titan-artifact-sphere x5'] },
     { date:'2026-08-14', type:'link', giftId:'d611c7bba5cb30a5af08b6309375d37b', note:'level',
