b0d187c0e96795af4d06e28e8ce95c6669f0949a
Alexandre221178 <133829010+Alexandre221178@users.noreply.github.com>
2026-09-02 15:27:00 -0300

    Add new gift entry for September 2, 2026-tarde

--- hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
+++ hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
@@ -9,8 +9,10 @@
   // 2026-09-##── 2 Link Exclusivo Web/FB ──────────────────────────────
 { date:'2026-09-##', type:'link', giftLinkWeb:'https://herowars.me/#wb#', giftLinkFb:'https://herowars.me/#fb#',
  note:'until|2026-09-##T02:00:00Z', rewards:['energy x500'] },
-https://www.hero-wars.com/?nx_source=group_posting_boxweb&gift_id=
+68f91a73df1791d852df492d51bda669
 */window.HWDE_GIFTS = [
+    { date:'2026-09-02', type:'link', giftId:'68f91a73df1791d852df492d51bda669', note:'level',
+  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
     { date:'2026-09-01', type:'link', giftId:'f8531a5c3540a59e2abea2e195aac57b', note:'level',
   rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
   { date:'2026-09-01', type:'link', giftLinkWeb:'https://herowars.me/3SImFFx', giftLinkFb:'https://herowars.me/4gFEih9',
