37e7775f6592d32affebbe8dfe570d0cced9daef
Alexandre221178 <133829010+Alexandre221178@users.noreply.github.com>
2026-09-03 14:23:21 +0000

    Add special gift entry for September 3, 2026-2-3
    Added a new special gift entry with specific rewards.

--- hero-wars-alliance/ag-daily-gifts-hwa/gifts-hwa-data.js
+++ hero-wars-alliance/ag-daily-gifts-hwa/gifts-hwa-data.js
@@ -3,6 +3,10 @@
     rewards:['silver-casket x1', 'energy x20', 'realm-casket x1'] },
 */
 window.HWA_GIFTS = [
+ { date:'2026-09-03', type:'special', mission:'2-3', note:'until|2026-09-28T01:00:00Z',
+    rewards:['summoning-sphere x5', 'gold 250k', 'energy x20' 'avatar x1'] },
+
+    
   { date:'2026-09-03', type:'daily', mission:'2-13', note:'daily|80',
     rewards:['silver-casket x1', 'energy x20', 'realm-casket x1'] },
   { date:'2026-09-02', type:'daily', mission:'3-14', note:'daily|80',
