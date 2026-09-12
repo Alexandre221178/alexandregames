bd4cfb33cbcd6fd83b3966f233a5678f07839b7a
Alexandre221178 <alexandredomingos2211@gmail.com>
2026-09-12 15:37:46 -0300

    wakeup

--- hero-wars-alliance/ag-daily-gifts-hwa/gifts-hwa-data.js
+++ hero-wars-alliance/ag-daily-gifts-hwa/gifts-hwa-data.js
@@ -3,6 +3,8 @@
     rewards:['silver-casket x1', 'energy x20', 'realm-casket x1'] },
 */
 window.HWA_GIFTS = [
+  { date:'2026-09-12', type:'hub', code:'WAKEUP', expires:'2026-09-28', rewards:['energy x100', 'large-skin-stone-chest x5'] },   
+
   { date:'2026-09-12', type:'daily', mission:'3-8', note:'daily|80',
     rewards:['silver-casket x1', 'energy x20', 'realm-casket x1'] },
      { date:'2026-09-11', type:'special', mission:'3-13', note:'until|2026-09-28T01:00:00Z',   
