092e94e169b4013069a522815c9c0ad7c79a475c
Alexandre221178 <133829010+Alexandre221178@users.noreply.github.com>
2026-09-18 18:05:50 +0000

    Add new daily gifts for September 2026-daily2

--- hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
+++ hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
@@ -11,6 +11,8 @@
  note:'until|2026-09-##T02:00:00Z', rewards:['energy x500'] },
 https://www.hero-wars.com/?nx_source=group_posting_boxweb&gift_id=
 */window.HWDE_GIFTS = [
+   { date:'2026-09-18', type:'link', giftId:'2a1d9fd16568f37e96865572393169fd', note:'level',
+  rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }] },
      { date:'2026-09-17', type:'link', giftId:'7324d972e20eac56f9e76bcfd35a4fe8', note:'level',
   rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'divine-card x5', 'energy x200', 'portal-charge x1' ] },
   { date:'2026-09-17', type:'link', giftLinkWeb:'https://herowars.me/46xxHkf', giftLinkFb:'https://herowars.me/46pVPp1',
