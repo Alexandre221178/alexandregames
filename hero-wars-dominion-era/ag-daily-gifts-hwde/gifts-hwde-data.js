636fdab790c6621c51a1fd72a96c37a388fde286
Alexandre221178 <alexandredomingos2211@gmail.com>
2026-09-26 07:38:29 -0300

    giftshwde

--- hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
+++ hero-wars-dominion-era/ag-daily-gifts-hwde/gifts-hwde-data.js
@@ -11,6 +11,10 @@ https://www.hero-wars.com/?nx_source=group_posting_giftweb&gift_id=
  note:'until|2026-09-##T02:00:00Z', rewards:['energy x500'] },
 https://www.hero-wars.com/?nx_source=group_posting_boxweb&gift_id=
 */window.HWDE_GIFTS = [
+{ date:'2026-09-26', type:'link', giftLinkWeb:'https://herowars.me/4dxpIHI', giftLinkFb:'https://herowars.me/3TpQ1c6',
+ note:'until|2026-10-01T02:00:00Z', rewards:['energy-crystal x16', 'valor-coin x8000', 'sapphire-medallion x1' ] },
+
+
  { date:'2026-09-25', type:'link', giftId:'338d8a5fa9b4b27f1c283e6ac22fe763', note:'level',
    rewards:[{ web:'platinum-box x3', fb:'fb-platinum-box x3' }, 'divine-card x5', 'energy x200', 'portal-charge x1' ] },
     { date:'2026-09-24', type:'link', giftId:'27e20c3eb42a9e7d003537cc1edd8cf2', note:'level',
